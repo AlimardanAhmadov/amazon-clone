c1404ea73414b037bbbe18a1126086ca9ff9fadd
Alimardan <alimardan_akhmedov@hotmail.com>
2022-10-12 21:03:37 +0400

    deployment --> back-end&front-end

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51LYpbOCX5IENONQwZaaf5aj6x6dUpQQTMgOllX7eLMtgpqjlRouVfRSGKLl4kLobRyHY2uDfnH3eF9BZbYNqO1ol00zEY1mwfh"
+  "***"
 );
 
 const app = express();
